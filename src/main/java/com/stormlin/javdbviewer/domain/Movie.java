1c62c31e0ceee33a1e122411455537058b66133d
stormlin <lin-jinting@outlook.com>
2023-07-23 15:25:11 +0800

    新增看过人数、想看人数和所有字段的注释

--- src/main/java/com/stormlin/javdbviewer/domain/Movie.java
+++ src/main/java/com/stormlin/javdbviewer/domain/Movie.java
@@ -7,10 +7,36 @@ import lombok.Data;
  */
 @Data
 public class Movie {
+    /**
+     * 番号
+     */
     String code;
+    /**
+     * 页面地址
+     */
     String url;
+    /**
+     * 片名
+     */
     String title;
+    /**
+     * 得分
+     */
     float score;
+    /**
+     * 投票人数
+     */
     int count;
+    /**
+     * 日期
+     */
     String date;
+    /**
+     * 看过人数
+     */
+    int seen;
+    /**
+     * 想看人数
+     */
+    int want;
 }
